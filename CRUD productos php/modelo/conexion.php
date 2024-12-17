8ef14130710c83e70117cd0a86a88bf9c2d6a662
IngCarranza <mauricarranza1010@gmail.com>
2024-12-16 18:57:57 -0600

    php

--- CRUD productos php/modelo/conexion.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conexion=new mysqli("localhost","root","","crud_php");
-$conexion->set_charset('utf8');
-?>
--- CRUD usuarios PHP/modelo/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conexion = new mysqli("localhost","root","","practica");
-$conexion->set_charset('utf8');
-
-?>
